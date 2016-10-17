34a2eccbc66b5263d637fb55bfb8fe0ea9680a02
Jonathon Chappell <jc300329@ad.jcu.edu.au>
2016-10-17 14:35:55 +1000

    Fixed debug errors for ch13

--- CodeInFigures/ReadEmployeeFile.java
+++ CodeInFigures/ReadEmployeeFile.java
@@ -1,11 +1,13 @@
 import java.nio.file.*;
 import java.io.*;
+import java.util.Scanner;
 public class ReadEmployeeFile
 {
    public static void main(String[] args)
    {
       Path file =
          Paths.get("C:\\Java\\Chapter.13\\Employees.txt");
+      String s = "";
       try
       {
          InputStream input = new BufferedInputStream(Files.newInputStream(file));
